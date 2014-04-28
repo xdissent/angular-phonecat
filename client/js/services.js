265f4f1f40b93698afba625a34e8af6fc3fda57d
Greg Thornton <xdissent@me.com>
2014-04-28 02:39:35 -0500

    Replace ngResource with meteorResource

--- client/js/services.js
+++ client/js/services.js
@@ -2,11 +2,9 @@
 
 /* Services */
 
-var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+var phonecatServices = angular.module('phonecatServices', ['meteorResource']);
 
-phonecatServices.factory('Phone', ['$resource',
-  function($resource){
-    return $resource('phones/:phoneId.json', {}, {
-      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-    });
+phonecatServices.factory('Phone', ['meteorResource',
+  function(meteorResource){
+    return meteorResource(Phones);
   }]);
